ef5edfe7b68106eadcd7669ac2c634cff9d23ba9
MariusMK1 <marius4556@gmail.com>
2022-06-08 21:33:57 +0300

    Started working on TodoAPI

---
+++ TodoAPI/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoAPI.Models;
+
+namespace TodoAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Todo> Todo { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
