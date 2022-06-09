3cff5379c9ec8ed0d695ea24e3440ebe9b5ae87a
MariusMK1 <marius4556@gmail.com>
2022-06-09 18:35:51 +0300

    Added CRUD operations to TodoAPI

--- TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/Controllers/TodoController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoAPI.Models;
+using TodoAPI.Services;
 
 namespace TodoAPI.Controllers
 {
@@ -11,6 +13,36 @@ namespace TodoAPI.Controllers
     [Route("[controller]")]
     public class TodoController : ControllerBase
     {
+        private readonly TodoService _todoService;
 
+        public TodoController(TodoService todoService)
+        {
+            _todoService = todoService;
+        }
+        [HttpGet]
+        public List<Todo> GetAll()
+        {
+            return _todoService.GetAll();
+        }
+        [HttpGet("{id}")]
+        public Todo Get(int id)
+        {
+            return _todoService.Get(id);
+        }
+        [HttpPost]
+        public void Create(string name)
+        {
+            _todoService.Add(name);
+        }
+        [HttpPut]
+        public void Update(Todo todo)
+        {
+            _todoService.Update(todo);
+        }
+        [HttpDelete("{id}")]
+        public void Remove(int id)
+        {
+            _todoService.Remove(id);
+        }
     }
 }
--- TodoAPI/Services/TodoService.cs
+++ TodoAPI/Services/TodoService.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoAPI.Data;
+using TodoAPI.Models;
 
 namespace TodoAPI.Services
 {
     public class TodoService
     {
+        private readonly DataContext _dataContext;
 
+        public TodoService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public List<Todo> GetAll()
+        {
+            return _dataContext.Todo.ToList();
+        }
+        public Todo Get(int id)
+        {
+            return _dataContext.Todo.FirstOrDefault(x => x.Id == id);
+        }
+        public void Add(string name)
+        {
+            Todo todo = new Todo{
+                Name = name,
+                CreatedDate = DateTime.Now,
+            };
+            _dataContext.Todo.Add(todo);
+            _dataContext.SaveChanges();
+        }
+        public void Update(Todo todo)
+        {
+            _dataContext.Update(todo);
+            _dataContext.SaveChanges();
+        }
+        public void Remove(int id)
+        {
+            var todo = Get(id);
+            _dataContext.Todo.Remove(todo);
+            _dataContext.SaveChanges();
+        }
     }
 }
